d4c004a2d3bcee36ba909b9400b7c0cebebe7888
gvalenciam <gvalenciam@pucp.pe>
2021-06-27 20:26:04 -0500

    Se agrego logica para abrir pagina de investigadores adjuntos en lugar de pagina por defecto. Se cambio imagen de la chica de espaldas en inicio

--- investigadores.php
+++ investigadores.php
@@ -2,13 +2,13 @@
 /*Template Name: Investigadores*/
 get_header(); ?>
 <div class="main-container seccion-centrada">
-        <?php while ( have_posts() ) : the_post(); ?>
-        <div class="w100 tac">
-            <h2 class="title"><?php echo get_the_title();?></h2>
-        </div>
-        <section  class="estrecho grid cx">                    
-            <p><?php the_content() ?></p>
-            <div class="w100 grid cx">
+    <?php while ( have_posts() ) : the_post(); ?>
+    <div class="w100 tac">
+        <h2 class="title"><?php echo get_the_title();?></h2>
+    </div>
+    <section class="estrecho grid cx">
+        <p><?php the_content() ?></p>
+        <div class="w100 grid cx">
             <?php
             $terms = get_terms( array(
                 'taxonomy' => 'tipo',
@@ -20,12 +20,12 @@ get_header(); ?>
 
             if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
             ?>
-                <?php
+            <?php
                 foreach ( $terms as $term ) {
                     ?><div class="w100 tac">
-                    <h3 class="title"><?php echo $term->name ?> </h3>
-                    </div>
-                    <?php
+                <h3 class="title"><?php echo $term->name ?> </h3>
+            </div>
+            <?php
                                 	
                     $args = array(
                         'post_type' => 'investigadores-post',
@@ -35,45 +35,46 @@ get_header(); ?>
                         $query = new WP_Query( $args );
                         
                         ?>
-                        <?php if ( $query->have_posts() ) : ?>
+            <?php if ( $query->have_posts() ) : ?>
 
-                        <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-                            <a class="persona grid cy cx" href="<?php the_permalink(); ?>">
-                                <div class="image">
-                                    <img src="<?php echo ipq_get_theme_image_url( get_post_thumbnail_id(), array( 170, 170, true ) ); ?>"
-                                        alt="<?php echo get_the_title();?>">
-                                </div>
-                                <div class="descripcion grid cy">
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+            <a class="persona grid cy cx"
+                href="<?php echo $term->slug != 'investigadores-adjuntos' ? the_permalink() : get_post_meta( get_the_ID(), '_investigador_link_institucion', 1); ?>">
+                <div class="image">
+                    <img src="<?php echo ipq_get_theme_image_url( get_post_thumbnail_id(), array( 170, 170, true ) ); ?>"
+                        alt="<?php echo get_the_title();?>">
+                </div>
+                <div class="descripcion grid cy">
 
-                                    <h3 class="w100"><?php echo get_the_title();?></h3>
-                                    <h4 class="w100"><?php echo get_post_meta( get_the_ID(), '_investigador_titulo', 1 ); ?></h4>
-                                
-                                    <?php if ($term->slug != 'investigadores-adjuntos') { ?>
+                    <h3 class="w100"><?php echo get_the_title();?></h3>
+                    <h4 class="w100"><?php echo get_post_meta( get_the_ID(), '_investigador_titulo', 1 ); ?></h4>
 
-                                        <p class="w100"><?php echo get_post_meta( get_the_ID(), '_investigador_correo', 1 ); ?></p>
+                    <?php if ($term->slug != 'investigadores-adjuntos') { ?>
 
-                                    <?php } ?>
+                    <p class="w100"><?php echo get_post_meta( get_the_ID(), '_investigador_correo', 1 ); ?></p>
 
-                                    <?php if ($term->slug == 'investigadores-adjuntos') { ?>
+                    <?php } ?>
 
-                                        <p class="w100"><?php echo get_post_meta( get_the_ID(), '_investigador_institucion', 1 ); ?></p> 
-                     
-                                    <?php } ?>
-                                    
-                                </div>
-                            </a>
-                        <?php endwhile;?>                               
-                        <?php endif; ?>
+                    <?php if ($term->slug == 'investigadores-adjuntos') { ?>
 
-                    <?php 
+                    <p class="w100"><?php echo get_post_meta( get_the_ID(), '_investigador_institucion', 1 ); ?></p>
+
+                    <?php } ?>
+
+                </div>
+            </a>
+            <?php endwhile;?>
+            <?php endif; ?>
+
+            <?php 
                 }
                 } ?>
- 
-                </div>
+
+        </div>
 
 
-                
-        </section>
-        <?php endwhile; // end of the loop. ?>
-    </div>
-    <?php get_footer(); ?>
+
+    </section>
+    <?php endwhile; // end of the loop. ?>
+</div>
+<?php get_footer(); ?>
